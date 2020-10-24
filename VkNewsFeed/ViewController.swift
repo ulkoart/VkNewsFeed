0d86354a886efd1150d1031f2700b875cd56838b
ulkoart <ulkoart@gmail.com>
2020-10-24 13:58:33 +0300

    Initial Commit

---
+++ VkNewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VkNewsFeed
+//
+//  Created by user on 24.10.2020.
+//  Copyright © 2020 ulkoart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
